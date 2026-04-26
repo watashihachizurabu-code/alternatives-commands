dc5bf45dde814738595109a0b79df067e0922640
watashihachizurabu-code <watashihachizurabu@gmail.com>
2026-04-25 19:18:49 -0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -3,7 +3,7 @@ _G.Whitelist = {
   ["Allenlovescars"] = 6, --MY KINGGGGGGG :weary:
   ["magicappelkz"] = 5,
   ["perimares2"] = 5,
-  ["Expired_Unit13"] = 5,
+  ["ChiefInquisitor"] = 5,
   ["JJEREMIAHJOHNSONN"] = 1,
   ["inkanzia"] = 5
 }
